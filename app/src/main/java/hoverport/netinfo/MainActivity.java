6536644ca32aa721faa8533f2605baf461f45231
YupeiZhu96 <yuz310@ucsd.edu>
2018-03-06 15:57:26 -0800

    Update MainActivity.java

--- app/src/main/java/hoverport/netinfo/MainActivity.java
+++ app/src/main/java/hoverport/netinfo/MainActivity.java
@@ -3,6 +3,8 @@ package hoverport.netinfo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.ActivityCompat;
@@ -38,18 +40,18 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private boolean  permission= false;
     private Thread thread;
-    private String t;
+    private static String t;
     private TextView textView = null;
     private PrintWriter printWriter;
-    private BufferedReader in;
-    private ExecutorService mExecutorService = null;
-    private String receiveMsg;
     private static String ips="";
     Handler textViewHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == 1 && textView!=null) {
-                //textView.setText(t);
+                textView.setText(t);
+            }
+            else if(msg.what == 0 && textView!=null){
+                textView.setText("Please enable USB tethering");
             }
             super.handleMessage(msg);
         }
@@ -74,69 +76,90 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ||ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_WIFI_STATE}, 0);
         }else{
             permission = true;
         }
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
-        mExecutorService = Executors.newCachedThreadPool();
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 Socket socket = null;
 
+                while(!getUSBTetheredIP()){
+                    Message msg =textViewHandler.obtainMessage();
+                    msg.what = 0;
+                    msg.sendToTarget();
+                }
+
                 while (true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
                     }
                     if (!permission) continue;
             /* first you wanna get a telephony manager by asking the system service */
                     TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-
+                    WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             /* then you can query for all the neighborhood cells */
-                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            ||ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
                         List<CellInfo> cellInfo = tm.getAllCellInfo();
                         t = "";
+                        WifiInfo wifiInfo = wm.getConnectionInfo();
+                        String wifiName = wifiInfo.getSSID();
+                        int wifiRssi = wifiInfo.getRssi();
+                        String name = tm.getNetworkOperatorName(), type="NULL";
+                        name = name.equals("") ? "NULL" : name;
+                        int cid = 0, strength = 0;
+                        //t += wifiInfo.toString();
+                        //t += "\n";
                         for (int i = 0; i < cellInfo.size(); i++) {
-                            int cid = 0, strength = 0;
-
                             if(cellInfo.get(i).isRegistered()) {
+                                String message = "";
+                                message+=tm.getNetworkOperatorName();
+                                message+="\n";
                                 if (cellInfo.get(i) instanceof CellInfoWcdma) {
                                     CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfo.get(i);
                                     strength = cellInfoWcdma.getCellSignalStrength().getDbm();
                                     cid = cellInfoWcdma.getCellIdentity().getCid();
                                     Log.wtf("wcdma", cellInfoWcdma.toString());
-                                    t += cellInfoWcdma.toString();
+                                    type="wcdma";
+                                    //t += cellInfoWcdma.toString();
                                 } else if (cellInfo.get(i) instanceof CellInfoGsm) {
                                     CellInfoGsm cellInfogsm = (CellInfoGsm) cellInfo.get(i);
                                     strength = cellInfogsm.getCellSignalStrength().getDbm();
                                     cid = cellInfogsm.getCellIdentity().getCid();
                                     Log.wtf("gsm", cellInfogsm.toString());
-                                    t += cellInfogsm.toString();
+                                    type="gsm";
+                                    //t += cellInfogsm.toString();
                                 } else if (cellInfo.get(i) instanceof CellInfoLte) {
                                     CellInfoLte cellInfoLte = (CellInfoLte) cellInfo.get(i);
                                     strength = cellInfoLte.getCellSignalStrength().getDbm();
                                     cid = cellInfoLte.getCellIdentity().getPci();
                                     Log.wtf("lte", cellInfoLte.toString());
-                                    t += cellInfoLte.toString();
+                                    type="lte";
+                                    //t += cellInfoLte.toString();
                                 } else if (cellInfo.get(i) instanceof CellInfoCdma) {
                                     CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfo.get(i);
                                     strength = cellInfoCdma.getCellSignalStrength().getCdmaDbm();
                                     cid = cellInfoCdma.getCellIdentity().getNetworkId();
                                     Log.wtf("cdma", cellInfoCdma.toString());
-                                    t += cellInfoCdma.toString();
+                                    type="cdma";
+                                    //t += cellInfoCdma.toString();
                                 }
-                                t += "\n";
+                                //t += "\n";
                             }
                         }
+                        t=wifiName+"\n"+wifiRssi+"\n"+name+" "+type+"\n"+strength+"\n";
                         Message msg =textViewHandler.obtainMessage();
                         msg.what = 1;
                         msg.sendToTarget();
                         try{
-                            socket = new Socket(getUSBTetheredIP(),5099);
+                            socket = new Socket(ips,5099);
                             printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
                                     socket.getOutputStream(), "UTF-8")), true);
                             printWriter.println(t);
@@ -151,34 +174,28 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             }
         });
         thread.start();
-        Log.wtf("ip",getUSBTetheredIP());
-        textView.setText(getUSBTetheredIP());
+        textView.setText(ips);
     }
-    public static String getUSBTetheredIP() {
+    public static boolean getUSBTetheredIP() {
 
         BufferedReader bufferedReader = null;
 
-        String test ="";
-
         try {
             bufferedReader = new BufferedReader(new FileReader("/proc/net/arp"));
-
+            //t = "";
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                Log.wtf("haha",line);
                 String[] splitted = line.split(" +");
-                if (splitted != null && splitted.length >= 4) {
+                if ( splitted.length >= 4) {
                     String ip = splitted[0];
-                    test = test+ip;
-                    test = test+"\n";
+                    //t+=ip;
+                    //t+="\n";
                     String mac = splitted[3];
                     if (mac.matches("..:..:..:..:..:..")) {
-                        if (mac.matches("00:00:00:00:00:00")) {
-                            //Log.d("DEBUG", "Wrong:" + mac + ":" + ip);
-                        } else if(ip.matches("192.168.42...")) {
+                            if(ip.matches("192.168.42...")) {
                             //Log.d("DEBUG", "Correct:" + mac + ":" + ip);
                             ips = ip;
-                            break;
+                            return true;
                         }
                     }
                 }
@@ -195,6 +212,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 e.printStackTrace();
             }
         }
-        return ips.equals("")?test:ips;
+        return false;
     }
 }
